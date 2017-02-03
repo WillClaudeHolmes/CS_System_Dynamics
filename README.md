37b36f3865a4da45adf2c7f6dd6814cbb24d7a54
WillClaudeHolmes <will.claude.holmes@gmail.com>
2017-02-03 00:40:18 -0600

    Module 2-5 almost ready.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 This learning module accompanies chapter two of Angela B. and George W Shiflet's
-book [Introduction to Computational Science](https://ics.wofford-ecs.org/).
+book [Introduction to Computational Science](https://ics.wofford-ecs.org/).  In this folder are:
+- [Notebook for Module 2-3](Module_2_3.ipynb) which discusses constrained exponential growth and introduces
+the concept of carrying capacity.
+- [Notebook for Module 2-5](Module_2_5.ipynb) which introduces multicompartment models as well as
+system inputs which are impulsive and time varying.
+- [Assignment Notebook](Assignment.ipynb) which gives the assignment for this chapter.
+
+Note also, that the Module 2-3 notebook contains [discussion of plotting with matplotlib](Module_2_3.ipynb#Note-on-Plotting).
--- SDExamples.py
+++ SDExamples.py
@@ -86,11 +86,26 @@ def multiple_dose_drug(dt, timemax, dose, interval, halflife, alpha):
     drug_in_system_array = [drug_in_system]
 
     while t < timemax:
-        if t % interval < dt:
-            drug_in_system += alpha * dose
+        if t % interval < dt:                         #if remainder of t/interval is smaller than dt then
+            drug_in_system += alpha * dose                #add impulse of drug to system.
         drug_in_system -= EliminationConstant * drug_in_system * dt
         t += dt
         t_array.append(t)
-        drug_in_system_array.append(drug_in_system/3000)
+        drug_in_system_array.append(drug_in_system)
 
     return t_array, drug_in_system_array
+
+def format_plot(a):
+    """Format a plot for presenation.  Function takes one parameter, a matplotlib.axes object."""
+
+    goldenratio = 1 / 2 * (1 + math.sqrt(5))  # The next few lines are used for the size of plots
+    fsx = 7  # Width (in inches) for the figures.
+    fsy = fsx / goldenratio  # Height (in inches) for the figures.
+
+    a.tick_params(labelsize=16)
+    a.xaxis.label.set_size(18)
+    a.yaxis.label.set_size(18)
+    a.title.set_size(20)
+    a.legend(fontsize=16)
+    a.get_figure().set_size_inches(fsx, fsy)
+    a.grid(1)
